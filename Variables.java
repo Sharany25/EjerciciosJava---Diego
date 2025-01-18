3adc535ec745a69338a9f0c295369a7d7e9dfe33
Sharany25 <115199969+Sharany25@users.noreply.github.com>
2025-01-18 13:28:53 -0600

    Ejercicios de Java Diego

---
+++ Variables.java
@@ -0,0 +1,18 @@
+package org.example.src;
+
+public class Variables {
+    public static void main(String[] args) {
+        boolean boleanVar=true;
+        System.out.println(boleanVar);
+
+        int initValue;
+        initValue=100;
+        System.out.println(initValue);
+
+        long hugeValue=10000000L;
+        System.out.println(hugeValue);
+
+        float floatVar=3.14f;
+        System.out.println(floatVar);
+    }
+}
