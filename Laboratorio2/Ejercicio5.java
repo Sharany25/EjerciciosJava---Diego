7e229bc89e6b835b4ca45f5cead7bbe282a2e1df
Sharany25 <115199969+Sharany25@users.noreply.github.com>
2025-01-25 08:06:27 -0600

    Add files via upload
    Ejercicios 2

---
+++ Laboratorio2/Ejercicio5.java
@@ -0,0 +1,19 @@
+package Laboratorio2;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        int loops = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el número de ciclos requeridos: ");
+        loops = sc.nextInt();
+
+        for (int count = 1; count <= loops; count++) {
+            System.out.println("Número actual de iteración: " + count);
+        }
+
+        sc.close();
+    }
+}
